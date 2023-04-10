bacd44c8b854614854253621848b7d53640c6e0d
Daniel Marschall <info@daniel-marschall.de>
2023-04-10 12:12:27 +0200

    Remove and exclude phpStan temp files

--- .phpstan/cache/PHPStan/16/83/1683e20a35c40eebfb2f193e9191d39b659552ab.php
+++
@@ -1,6 +0,0 @@
-<?php declare(strict_types = 1);
-
-return PHPStan\Cache\CacheItem::__set_state(array(
-   'variableKey' => '1681076633-v4',
-   'data' => false,
-));
--- .phpstan/cache/PHPStan/a1/54/a1541bb9fd2051c5f4495c69e77f4542a7232380.php
+++
@@ -1,6 +0,0 @@
-<?php declare(strict_types = 1);
-
-return PHPStan\Cache\CacheItem::__set_state(array(
-   'variableKey' => '1681076627-v4',
-   'data' => false,
-));
